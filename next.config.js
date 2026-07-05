b9696b76d088589d2e03842a5e092535b64c8fa3
Kiltipedya <kiltipedya.1804@gmail.com>
2026-07-05 20:00:49 +0300

    fix: add Wikimedia domains to next/image config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'res.cloudinary.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'upload.wikimedia.org' },
+      { protocol: 'https', hostname: '*.wikipedia.org' },
     ],
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 60 * 60 * 24 * 30, // 30 jours
